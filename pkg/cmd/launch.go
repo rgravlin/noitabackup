0acaa2f3a8b3911efff1763985af61b8d25ed421
Ryan Gravlin <ryan.gravlin@gmail.com>
2024-06-02 23:52:11 -0400

    Cleanup descriptions, use correct default GUI boot mode

--- pkg/cmd/launch.go
+++ pkg/cmd/launch.go
@@ -13,13 +13,8 @@ import (
 // launchCmd represents the launch command
 var launchCmd = &cobra.Command{
 	Use:   "launch",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Launch the Noita game from Steam",
+	Long:  `Launches the Noita Steam game`,
 	Run: func(cmd *cobra.Command, args []string) {
 		err := lib.LaunchNoita()
 		if err != nil {
@@ -30,14 +25,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(launchCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// launchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// launchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
