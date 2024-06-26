c650efa09aeb1f671defff2475b1e832554a8696
Ryan Gravlin <ryan.gravlin@gmail.com>
2024-06-26 05:57:29 -0400

    Update configuration error UI

--- pkg/cmd/utils.go
+++ pkg/cmd/utils.go
@@ -16,44 +16,48 @@ func validateCommandOptions(cmd *cobra.Command, args []string) error {
 
 	numBackups := viper.GetInt(internal.ViperNumBackups)
 	if numBackups > ConfigMaxNumBackupsToKeep || numBackups <= 0 {
-		uiErr = fmt.Sprintf("%s: %d", internal.ErrNumBackups, viper.GetInt(internal.ViperNumBackups))
+		uiErr = fmt.Sprintf("%s: %d", internal.ErrNumBackups, numBackups)
 	}
 
 	if path, err := internal.GetSourcePath(viper.GetString(internal.ViperSourcePath)); err != nil {
-		uiErr = fmt.Sprintf("%s: %v", internal.ErrGettingSourcePath, err)
+		uiErr = fmt.Sprintf("%v", err)
 	} else {
 		viper.Set(internal.ViperSourcePath, path)
 	}
 
 	if path, err := internal.GetDestinationPath(viper.GetString(internal.ViperDestinationPath)); err != nil {
-		uiErr = fmt.Sprintf("%s: %v", internal.ErrGettingDestinationPath, err)
+		uiErr = fmt.Sprintf("%v", err)
 	} else {
 		viper.Set(internal.ViperDestinationPath, path)
 	}
 
 	if path, err := internal.GetSteamPath(viper.GetString(internal.ViperSteamPath)); err != nil {
-		uiErr = fmt.Sprintf("%s: %v", internal.ErrSteamPathNotExist, err)
+		uiErr = fmt.Sprintf("%v", err)
 	} else {
 		viper.Set(internal.ViperSteamPath, path)
 	}
 
 	if uiErr != "" {
-		go func() {
-			window := new(app.Window)
-			window.Option(
-				app.Title(appName),
-				app.MaxSize(unit.Dp(internal.ErrorWidth), unit.Dp(internal.ErrorHeight)),
-				app.MinSize(unit.Dp(internal.ErrorWidth), unit.Dp(internal.ErrorHeight)),
-			)
-			ui := internal.NewErrorUI(uiErr)
-			err := ui.Run(window)
-			if err != nil {
-				log.Fatal(err)
-			}
-			os.Exit(0)
-		}()
-		app.Main()
+		RunErrorUI(uiErr)
 	}
 
 	return nil
+}
+
+func RunErrorUI(error string) {
+	go func() {
+		window := new(app.Window)
+		window.Option(
+			app.Title(appName),
+			app.MaxSize(unit.Dp(internal.ErrorWidth), unit.Dp(internal.ErrorHeight)),
+			app.MinSize(unit.Dp(internal.ErrorWidth), unit.Dp(internal.ErrorHeight)),
+		)
+		ui := internal.NewErrorUI(error)
+		err := ui.Run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
 }
--- pkg/internal/ui_error.go
+++ pkg/internal/ui_error.go
@@ -29,6 +29,14 @@ var (
 	colorBlack = color.NRGBA{A: 255}
 )
 
+func makeText(theme *material.Theme, in string) material.LabelStyle {
+	str := material.Body1(theme, in)
+	str.Color = colorBlack
+	str.Alignment = text.Middle
+
+	return str
+}
+
 // Run handles all the events and rendering for the application window.
 // It takes a *app.Window as a parameter and returns an error if any.
 func (ui *ErrorUI) Run(window *app.Window) error {
@@ -45,15 +53,24 @@ func (ui *ErrorUI) Run(window *app.Window) error {
 			paint.ColorOp{Color: color.NRGBA{A: 0xff, R: 0xff}}.Add(gtx.Ops)
 			paint.PaintOp{}.Add(gtx.Ops)
 
-			errorString := material.Body1(theme, ui.Error)
-			errorString.Color = colorBlack
-			errorString.Alignment = text.Middle
+			errorLabel := makeText(theme, InfoErrorMessage)
+			errorString := makeText(theme, ui.Error)
 
 			for quitButton.Clicked(gtx) {
 				os.Exit(0)
 			}
 
 			widgets := []layout.Widget{
+				func(gtx C) D {
+					in := layout.UniformInset(unit.Dp(0))
+					return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						return layout.Flex{Alignment: layout.Middle}.Layout(gtx,
+							layout.Rigid(func(gtx C) D {
+								return in.Layout(gtx, errorLabel.Layout)
+							}),
+						)
+					})
+				},
 				func(gtx C) D {
 					in := layout.UniformInset(unit.Dp(0))
 					return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
